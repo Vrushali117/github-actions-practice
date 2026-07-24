6975c3699c7a42ebc8eeab62285316c5a0e0c50b
Vrushali117 <71395348+Vrushali117@users.noreply.github.com>
2026-07-24 16:31:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
+# This code is from https://github.com/LondheShubham153/flask-app-ecs/tree/main
+# Flask App
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def hello_world():
-    return render_template("index.html")
+    return render_template('index.html')
 
 
-@app.route("/health")
+@app.route('/health')
 def health():
-    return "Server is up and running"
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return 'Server is up and running'
